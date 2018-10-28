2ff3380332cf14b0e02965be5ca258356cbaca3b
hariharan <31499070+Hariharanu@users.noreply.github.com>
2018-10-28 22:30:15 +0530

    Create p.py

---
+++ p.py
@@ -0,0 +1,18 @@
+import n
+def he():
+	n=input("what do you want from me?...")
+def re():
+	n1=input()
+	if n1=="what are you doing" or n1== "what doing":
+		n.me()
+		he()
+def m():
+	r2=input()
+	if r2=="how are you" or r2== "HOW ARE YOU" or r2== "How Are you":
+		n.h()
+		re()
+n1=input()
+if n1=="HAi" or n1== "hai" or n1== "hello"  or n1== "HELLO" or  n1== "OII" or  n1== "oii" or  n1=='hi' or n1=='HI':
+	n.g()
+	m()
+
