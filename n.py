87e571dd3ee35966f8686ee2e43648a5436189ce
hariharan <31499070+Hariharanu@users.noreply.github.com>
2018-10-28 22:31:11 +0530

    Create n.py

---
+++ n.py
@@ -0,0 +1,13 @@
+import random
+def g():
+	a=["hello",'hai','hi','HAI','HELLO','HI']
+	print(random.choice(a))
+def what():
+	print("what")
+def h():
+	a=["fine",'Fine','good','Good','Not bad']
+	print(random. choice(a))
+def me():
+	n=input("just talking with you \n you?")
+	print("ok")
+
